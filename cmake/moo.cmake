e3356a8b0877aca61576277faf1c854db90168c3
Alessandro Thea <alessandro.thea@cern.ch>
2021-02-16 03:01:50 +0100

    Further compacting dependency handling code.

--- cmake/moo.cmake
+++ cmake/moo.cmake
@@ -20,16 +20,49 @@ set(MOO_CMD "moo" CACHE STRING "The 'moo' command")
 #   set("${prefix}_DEPS_NAME" "${DEPS_NAME}" PARENT_SCOPE)
 # endfunction()
 
+# ----
+# function(moo_deps_name deps_dir target source prefix)
+#   get_filename_component(basename ${source} NAME)
+#   string(REGEX REPLACE "[^a-zA-Z0-9]" "_" basename "${basename}")
+#   set("${prefix}_DEPS_TARGET" "${target}__${basename}_deps" PARENT_SCOPE)
+#   set("${prefix}_DEPS_FILE" "${deps_dir}/${target}__${basename}.d" PARENT_SCOPE)
+#   set("${prefix}_DEPS_PHONY" "${deps_dir}/${target}__${basename}.d.phony" PARENT_SCOPE)
+#   # set("${prefix}_DEPS_PHONY" "${${prefix}_DEPS_NAME}.phony" PARENT_SCOPE)
+# endfunction()
 
-function(moo_deps_name deps_dir target source prefix)
+# ----
+function(moo_update_deps base_args deps_dir main_target source target_prefix)
+
   get_filename_component(basename ${source} NAME)
   string(REGEX REPLACE "[^a-zA-Z0-9]" "_" basename "${basename}")
-  set("${prefix}_DEPS_TARGET" "${target}__${basename}_deps" PARENT_SCOPE)
-  set("${prefix}_DEPS_FILE" "${deps_dir}/${target}__${basename}.d" PARENT_SCOPE)
-  set("${prefix}_DEPS_PHONY" "${deps_dir}/${target}__${basename}.d.phony" PARENT_SCOPE)
-  # set("${prefix}_DEPS_PHONY" "${${prefix}_DEPS_NAME}.phony" PARENT_SCOPE)
+  set(DEPS_TARGET "${main_target}__${basename}_deps")
+  set(DEPS_FILE "${deps_dir}/${main_target}__${basename}.d")
+  set(DEPS_PHONY "${deps_dir}/${main_target}__${basename}.d.phony")
+
+  # moo_deps_name(${MC_DEPS_DIR} ${MC_TARGET} ${MC_CODEDEP} MC_CODEDEP)
+
+  set(DEPS_ARGS ${base_args} imports -o ${DEPS_FILE} ${source})
+  add_custom_command(
+      COMMAND ${MOO_CMD} ARGS ${DEPS_ARGS}
+      COMMAND bash ARGS -c "touch -r $(ls -t $(cat ${DEPS_FILE} ) | head -n1) ${DEPS_FILE}"
+      VERBATIM
+      COMMENT "Updating moo dependencies of ${source}"
+      OUTPUT ${DEPS_FILE}
+      OUTPUT ${DEPS_PHONY}
+  )
+
+  # # Custom target to force the update of jsonnet dependencies at build time
+  # # Note the phony dependency to force it to be re-run every time
+  add_custom_target(${DEPS_TARGET}
+      ALL
+      DEPENDS ${DEPS_FILE} ${DEPS_PHONY}
+  )
+
+  set(${target_prefix}_DEPS_TARGET ${DEPS_TARGET} PARENT_SCOPE)
 endfunction()
 
+
+#-----
 function(moo_render)
   cmake_parse_arguments(MC "" "TARGET;MODEL;TEMPL;CODEGEN;MPATH;TPATH;GRAFT;CODEDEP;DEPS_DIR" "TLAS" ${ARGN})
 
@@ -54,30 +87,11 @@ function(moo_render)
 
   set(MC_CODEGEN_ARGS ${MC_BASE_ARGS} render -o ${MC_CODEGEN} ${MC_MODEL} ${MC_TEMPL})
 
-  
-  moo_deps_name(${MC_DEPS_DIR} ${MC_TARGET} ${MC_CODEDEP} MC_CODEDEP)
+  moo_update_deps("${MC_BASE_ARGS}" ${MC_DEPS_DIR} ${MC_TARGET} ${MC_CODEDEP} MC_CODEDEP)
+  moo_update_deps("${MC_BASE_ARGS}" ${MC_DEPS_DIR} ${MC_TARGET} ${MC_TEMPL} MC_TEMPL)
 
-  set(MC_CODEDEP_DEPS_ARGS ${MC_BASE_ARGS} imports -o ${MC_CODEDEP_DEPS_FILE} ${MC_CODEDEP})
-  add_custom_command(
-      COMMAND ${MOO_CMD} ARGS ${MC_CODEDEP_DEPS_ARGS}
-      COMMAND bash ARGS -c "touch -r $(ls -t $(cat ${MC_CODEDEP_DEPS_FILE} ) | head -n1) ${MC_CODEDEP_DEPS_FILE}"
-      VERBATIM
-      COMMENT "Updating moo dependencies of ${MC_CODEDEP}"
-      OUTPUT ${MC_CODEDEP_DEPS_FILE}
-      OUTPUT ${MC_CODEDEP_DEPS_PHONY}
-  )
-
-  # # Custom target to force the update of jsonnet dependencies at build time
-  # # Note the phony dependency to force it to be re-run every time
-  add_custom_target(${MC_CODEDEP_DEPS_TARGET}
-      ALL
-      DEPENDS ${MC_CODEDEP_DEPS_FILE} ${MC_CODEDEP_DEPS_PHONY}
-  )
-  #--------------------
-
-  # message(NOTICE "${MC_TARGET} ${MC_BASE_ARGS} ${MC_CODEGEN_ARGS}")
   add_custom_command(OUTPUT ${MC_CODEGEN} COMMAND ${MOO_CMD} ${MC_CODEGEN_ARGS} DEPENDS ${MC_CODEDEP})
-  add_custom_target(${MC_TARGET} DEPENDS ${MC_CODEGEN} ${MC_CODEDEP_DEPS_TARGET})
+  add_custom_target(${MC_TARGET} DEPENDS ${MC_CODEGEN} ${MC_CODEDEP_DEPS_TARGET} ${MC_TEMPL_DEPS_TARGET})
 
 endfunction()
 
